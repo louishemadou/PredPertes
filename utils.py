8d7dc2041226d22e6af9b4356508cdadf55a86df
Louis Hémadou <louis.hemadou@eleves.enpc.fr>
2020-05-26 13:04:15 +0200

    r2 score

--- utils.py
+++ utils.py
@@ -7,10 +7,8 @@ from sklearn.metrics import r2_score
 def error(Y_pred, Y):
     """Measures accuracy of a
     regression prediction"""
-    error = np.abs(Y_pred - Y) / Y
     r2 = r2_score(Y, Y_pred)
     print("R² score: " + str(r2))
-    print(str(round(error.mean(), 4)) + " experimental error") # Mean experimental error
 
 def add_ones(X):
     """ Adds a column of ones in the beginning of X"""
