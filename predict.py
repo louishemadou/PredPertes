2b008fd91ee0af61d4e612caa6d3448c65c151d0
Louis Hémadou <louis.hemadou@eleves.enpc.fr>
2020-06-29 15:51:11 +0200

    nettoyage

--- predict.py
+++ predict.py
@@ -1,13 +1,9 @@
 from sklearn.metrics import r2_score
 from sklearn.svm import SVR
 from sklearn.decomposition import PCA
-from split import retrieve 
-from pca import standardize, normalize
 from visualize import compare
 from linear_r import LinearRegression
-from utils import error
-from svm import SAGRegressor
-from ksvm import poly_kernel, gauss_kernel, SDCARegressor
+from utils import *
 from rn_tf import *
 
 X_train, Y_train = retrieve(2016)
@@ -33,21 +29,21 @@ X_val_pca = pca.fit_transform(X_val_s)
 X_test_pca = pca.fit_transform(X_test_s)
 
 # Testing Linear_regression
-# With normalized data
 
+# With normalized data
 print("Linear regression with normalized data")
 LR = LinearRegression(lamb = 0.1, delta = 0.000015)
-LR.fit(X_train_n, Y_train, epochs = 1000, Visual = True)
+LR.fit(X_train_n, Y_train, epochs = 100, Visual = True)
 Y_pred_1 = LR.predict(X_val_n)
 Y_pred_2 = LR.predict(X_test_n)
 error(Y_pred_1, Y_val, "val")
 error(Y_pred_2, Y_test, "test")
 #compare(Y_val, Y_pred)
-# With standardized data
 
+# With standardized data
 print("Linear regression with standardized data")
 LR = LinearRegression(lamb = 0.1, delta = 0.00001)
-LR.fit(X_train_s, Y_train, epochs = 1000, Visual = True)
+LR.fit(X_train_s, Y_train, epochs = 100, Visual = True)
 Y_pred_1 = LR.predict(X_val_s)
 Y_pred_2 = LR.predict(X_test_s)
 error(Y_pred_1, Y_val, "val")
@@ -55,10 +51,9 @@ error(Y_pred_2, Y_test, "test")
 compare(Y_test, Y_pred_2, 2018)
 
 # With standardized and orthogonalized data
-
 print("Linear regression with standardized and orthogonalized data")
 LR = LinearRegression(lamb = 0.1, delta = 0.00001)
-LR.fit(X_train_pca, Y_train, epochs = 200, Visual = True)
+LR.fit(X_train_pca, Y_train, epochs = 100, Visual = True)
 Y_pred_1 = LR.predict(X_val_pca)
 Y_pred_2 = LR.predict(X_test_pca)
 error(Y_pred_1, Y_val, "val")
@@ -78,7 +73,6 @@ error(Y_pred_1, Y_val, "val")
 error(Y_pred_2, Y_test, "test")
 
 # With standardized data
-
 print("SVR with standardized data")
 svr = SVR(kernel = ker, tol = 1e-10, verbose = True)
 svr.fit(X_train_s, Y_train)
@@ -88,7 +82,6 @@ error(Y_pred_1, Y_val, "val")
 error(Y_pred_2, Y_test, "test")
 
 # With standardized and orthogonalized data
-
 print("SVR with standardized and orthogonalized data")
 svr = SVR(kernel = ker, tol = 1e-10, verbose = True)
 svr.fit(X_train_pca, Y_train)
@@ -98,13 +91,12 @@ error(Y_pred_1, Y_val, "val")
 error(Y_pred_2, Y_test, "test")
 
 # Testing NN
-
 layers = [400, 400, 100, 1]
 activations = ["sigmoid", "sigmoid", "relu", "linear"]
 input_dim = X_train.shape[1]
 lr = 0.00002
 net = Net(lr, input_dim, layers, activations)
-net.fit(X_train_s, Y_train, epochs = 50, batch_size = 50, Visual = False)
+net.fit(X_train_s, Y_train, epochs = 50, batch_size = 200, Visual = False)
 Y_pred_1 = net.predict(X_val_s)
 Y_pred_2 = net.predict(X_test_s)
 error(Y_pred_1, Y_val, "val")
--- selection.py
+++ selection.py
@@ -1,11 +1,10 @@
 from linear_r import LinearRegression
-from split import *
 from visualize import influence
-from pca import standardize
-from utils import add_ones
+from utils import *
 from scipy.stats import t
 import numpy as np
 import matplotlib.pyplot as plt
+
 X_2016, Y_2016 = retrieve(2016)
 X_2017, Y_2017 = retrieve(2017)
 X_2018, Y_2018 = retrieve(2018)
@@ -15,7 +14,7 @@ Y = np.concatenate((Y_2016, Y_2017, Y_2018))
 
 n, p = X.shape
 LR = LinearRegression(lamb=1000, delta=0.00000000000000005)
-LR.fit(X, Y, epochs=15, Visual=True)
+LR.fit(X, Y, epochs=100, Visual=True)
 Y_pred = LR.predict(X)
 
 w = LR.weights()
@@ -38,14 +37,13 @@ for i in range(p+1): # Formule du poly
 
 influe = []
 
-for i in range(p+1):
+for i in range(p+1): # Tests de Student
     print(w[i], labels[i])
     if i>0:
         influe.append(int(output[i][2]))
 
 Influence = [out[0] for out in output if out[2]]
 NInflience = [out[0] for out in output if not out[2]]
-print(influe)
 print("Variables ayant une influence: ", Influence)
 print(len(Influence))
 print("Variables n'ayant pas d'influence ", NInflience)
