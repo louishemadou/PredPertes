f2297c7b9280acd96fbb21f1241254692585582d
Louis Hémadou <louis.hemadou@eleves.enpc.fr>
2020-06-29 15:52:11 +0200

    nettoyage

--- rn_tf.py
+++ rn_tf.py
@@ -1,55 +1,7 @@
 import keras
+import matplotlib.pyplot as plt
 import tensorflow as tf
-import os
-import time
-from pca import *
-from utils import error
-from split import retrieve_and_split, retrieve_all_and_split, retrieve, sample_and_retrieve
-from visualize import compare, compare_nd
-
-
-X_train, Y_train = retrieve(2016)
-X_val, Y_val = retrieve(2017)
-
-X_train_n = normalize(X_train)
-X_val_n = normalize(X_val)
-
-m = X_train.shape[1]
-
-influe_student = [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
-to_select_student = []
-not_to_select_student = []
-for i in range(len(influe_student)):
-    if influe_student[i]:
-        to_select_student.append(i)
-    else:
-        not_to_select_student.append(i)
-
-doublons = [3, 5, 6, 8, 12, 30, 16]
-expl = [1, 2, 11, 22, 25]
-
-to_select_doublons = []
-to_select_expl = []
-to_select_doublon_expl = []
-for i in range(m):
-    if i not in doublons:
-        to_select_doublons.append(i)
-    if i not in expl:
-        to_select_expl.append(i)
-    if i not in doublons:
-        if i not in expl:
-            to_select_doublon_expl.append(i)
-
-
-X_train_2n = X_train_n[:, to_select_doublon_expl]
-X_val_2n = X_val_n[:, to_select_doublon_expl]
-# Testing PCA and creating modified sets
-
-
-
-X_train_n = normalize(X_train)
-X_val_n = normalize(X_val)
-
+from utils import *
 
 class Net():
     def __init__(self, lr, input_dim, layers, activations):
@@ -76,16 +28,3 @@ class Net():
 
     def predict(self, X):
         return self.model.predict(X)
-"""
-T0 = time.time()
-layers = [400, 400, 100, 1]
-activations = ["sigmoid", "sigmoid", "relu", "linear"]
-input_dim = X_train.shape[1]
-lr = 0.00001
-net = Net(lr, input_dim, layers, activations)
-net.fit(X_train_2n, Y_train, epochs = 100, batch_size = 100, Visual = False)
-Y_pred = net.predict(X_val_2n)
-error(Y_pred, Y_val)
-print(time.time() - T0)
-#compare(Y_pred, Y_val)
-"""
--- utils.py
+++ utils.py
@@ -1,8 +1,18 @@
 """Useful functions for algorithms"""
-
+import pandas as pd
 import numpy as np
 import os
 from sklearn.metrics import r2_score
+
+def standardize(X):
+    eps = 1e-8 # In case where std = 0
+    X_s = (X-np.mean(X, axis = 0))/(np.std(X, axis = 0) + eps)
+    return X_s
+
+def normalize(X):
+    eps = 1e-8 # In case where max = min
+    X_n = (X-np.min(X, axis=0))/(np.max(X, axis = 0)-np.min(X, axis=0)+eps)
+    return X_n
 
 def error(Y_pred, Y, set_name):
     """Measures accuracy of a
@@ -25,3 +35,78 @@ def path(name):
     if os.name != 'posix':
         return name.replace('/', '\\')
     return name
+
+
+def retrieve(year):
+    """Retrieve data of a single year"""
+    path_X = "./data/conso/conso_" + str(year) + ".csv"
+    path_Y = "./data/pertes/pertes_" + str(year) + ".csv"
+    X = pd.read_csv(path_X).to_numpy()
+    Y = pd.read_csv(path_Y).to_numpy()[:, 3]
+
+    return X, Y
+
+def retrieve_features():
+    """Return all features name"""
+    return pd.read_csv("./data/conso/conso_2015.csv").columns
+
+def retrieve_and_split(year):
+    """Retrieve and split data of a single year
+    into train/val/test datasets"""
+    size_1 = 0.70
+    size_2 = 0.85
+
+    path_X = "./data/conso/conso_" + str(year) + ".csv"
+    path_Y = "./data/pertes/pertes_" + str(year) + ".csv"
+    X = pd.read_csv(path_X).to_numpy()
+    Y = pd.read_csv(path_Y).to_numpy()[:, 3]
+
+    n = len(X)
+    n_1 = int(size_1*n)
+    n_2 = int(size_2*n)
+
+    r = np.random.permutation(n)
+
+    X_train = X[r[0:n_1], :]
+    Y_train = Y[r[0:n_1]]
+    X_val = X[r[n_1:n_2], :]
+    Y_val = Y[r[n_1:n_2]]
+    X_test = X[r[n_2:], :]
+    Y_test = Y[r[n_2:]]
+
+    return X_train, Y_train, X_val, Y_val, X_test, Y_test
+
+
+def retrieve_all_and_split():
+    """Retrieve and split data 
+    of 2016-2017-2018
+    into train/val/test datasets"""
+
+    path_train_X = "./data/conso/conso_2016.csv"
+    path_train_Y = "./data/pertes/pertes_2016.csv"
+    path_val_X = "./data/conso/conso_2017.csv"
+    path_val_Y = "./data/pertes/pertes_2017.csv"
+    path_test_X = "./data/conso/conso_2018.csv"
+    path_test_Y = "./data/pertes/pertes_2018.csv"
+    
+    X_train = pd.read_csv(path_train_X).to_numpy()
+    Y_train = pd.read_csv(path_train_Y).to_numpy()[:, 3]
+    X_val = pd.read_csv(path_val_X).to_numpy()
+    Y_val = pd.read_csv(path_val_Y).to_numpy()[:, 3]
+    X_test = pd.read_csv(path_test_X).to_numpy()
+    Y_test = pd.read_csv(path_test_Y).to_numpy()[:, 3]
+
+    return X_train, Y_train, X_val, Y_val, X_test, Y_test
+
+def sample_and_retrieve(n):
+    """returns n random samples
+    of all data"""
+    X_2016, Y_2016 = retrieve(2016)
+    X_2017, Y_2017 = retrieve(2017)
+    X_2018, Y_2018 = retrieve(2018)
+
+    X = np.concatenate((X_2016, X_2017, X_2018))
+    Y = np.concatenate((Y_2016, Y_2017, Y_2018))
+    r = np.random.permutation(len(Y))
+    return X[r[0:n],:], Y[r[0:n]]
+
